4b124705264a458dd60dc20e68ec509cf26dd067
Daniel Carvalho <danieljosecarvalho@gmail.com>
2022-07-08 20:48:06 -0300

    feat: first assembly code hello

--- boot_sector/boot_sector.asm
+++ boot_sector/boot_sector.asm
@@ -1,9 +1,39 @@
 ;
-;   A simple boot sector program that loops forever
+;   A simple boot sector that prints a message to the screen using a BIOS routine.
 ;
 
-loop:
-    jmp loop
+mov ah, 0x0E
+
+mov al, 'H'
+int 0x10
+mov al, 'e'
+int 0x10
+mov al, 'l'
+int 0x10
+mov al, 'l'
+int 0x10
+mov al, 'o'
+int 0x10
+mov al, ' '
+int 0x10
+mov al, 'c'
+int 0x10
+mov al, 'o'
+int 0x10
+mov al, 's'
+int 0x10
+mov al, 'm'
+int 0x10
+mov al, 'o'
+int 0x10
+mov al, 's'
+int 0x10
+
+jmp $
+
+;
+;   Padding and magic BIOS number
+;
 
 times 510-($-$$) db 0
 
